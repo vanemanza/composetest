09f86b191ea0f6c029b18e81a94a91051c1e4de5
Vanesa Manzanelli <vanemanza1@gmail.com>
2025-03-28 16:13:28 -0300

    Update greeting message and add volume mapping in docker-compose

--- app.py
+++ app.py
@@ -7,7 +7,7 @@ redis = Redis(host='redis', port=6379)
 @app.route('/')
 def hello():
     count = redis.incr('hits')
-    return 'Hello World! I have been seen {} times.\n'.format(count)
+    return 'Hello World! lalala I have been seen {} times.\n'.format(count)
 
 if __name__ == "__main__":
     app.run(host="0.0.0.0", debug=True)
